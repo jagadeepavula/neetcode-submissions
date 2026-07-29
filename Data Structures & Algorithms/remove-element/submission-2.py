c07f192ca53f90e07dbf6d593d2a2ff1c311cedb
jagadeep avula <84993949+jagadeepavula@users.noreply.github.com>
2026-07-29 14:16:46 -0700

    Add: remove-element - submission-2

---
+++ Data Structures & Algorithms/remove-element/submission-2.py
@@ -0,0 +1,11 @@
+class Solution:
+    def removeElement(self, nums: List[int], val: int) -> int:
+        k = 0 
+        for i in range(len(nums)):
+            if nums[i] == val:
+                nums[i] = 999
+                k += 1
+        nums.sort()
+        return len(nums)-k
+    
+
